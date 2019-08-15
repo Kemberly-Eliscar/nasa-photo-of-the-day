0be5be8ec5632e69acd0d8980ee9f2809540ea09
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-08-15 13:04:33 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 import "./App.css";
+import PhotoCard from './components/PhotoCard.js';
 
 function App() {
+  const [data, setData] = useState({});
+  
+  axios.get("view-source:https://api.nasa.gov/planetary/apod?api_key=RlVu0xWKQ2LGh5zKsgeSnpciQJvSxmA4e0OxOBqc")
+  .then(res => {
+    console.log(res.data);
+    setData(res.data);
+  });
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
+     <p>
+       Read through the instructions.
+     </p>
+     <PhotoCard title={data.title} 
+                url= {data.url}
+                explanation={data.explanation} />
+     </div>
+  
   );
 }
 
