c5c7864be25a205d7d25b3585eb726f3afac07fb
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-08-15 15:51:42 -0400

    hit mvp

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import "./App.css";
 import PhotoCard from './components/PhotoCard.js';
+import "./App.css";
 
 function App() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState(null);
   useEffect(() => {
-
-  }, [])
+    console.log('first render');
   
-  axios.get("https://api.nasa.gov/planetary/apod?api_key=RlVu0xWKQ2LGh5zKsgeSnpciQJvSxmA4e0OxOBqc")
+  axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-03-14")
   .then(res => {
     console.log(res.data);
     setData(res.data);
   });
+}, []);
   return (
     <div className="App">
      <p>
        Read through the instructions.
      </p>
-     <PhotoCard title={data.title} 
-                url= {data.url}
+     {data
+        ? <PhotoCard title={data.title} 
+                url={data.url}
                 explanation={data.explanation} />
-     </div>
-  
+     : <div>Loading</div>
+      }
+    </div>
   );
 }
-
 export default App;
